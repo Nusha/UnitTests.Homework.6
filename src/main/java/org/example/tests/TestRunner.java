dea97816db9df2b25c3fc3168dae67bd95fa60d5
Ivan Balabay <i.balabai@gmail.com>
2023-11-20 23:42:42 +0300

    Minor fix in CL UI

--- src/main/java/org/example/tests/TestRunner.java
+++ src/main/java/org/example/tests/TestRunner.java
@@ -6,7 +6,7 @@ public class TestRunner {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(ListAverageCalculatorTest.class, DoubleListAverageCalculatorTest.class, ListComparatorTest.class);
         for (Failure failure : result.getFailures()) {
-            System.out.println("\u2757 Тест не пройден: " + failure +"\n");
+            System.out.println("\u2757  Тест не пройден: " + failure +"\n");
         }
 
         System.out.println((result.wasSuccessful()) ? "Тесты пройдены успешно \u2705" : "\u274c Тесты провалены \u2191");
