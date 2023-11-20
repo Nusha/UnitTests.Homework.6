837161ab3aa3289eb6faed4385978903a3e67f3f
Ivan Balabay <i.balabai@gmail.com>
2023-11-20 23:13:40 +0300

    ListComparatorTest has been added;

---
+++ src/main/java/org/example/tests/ListComparatorTest.java
@@ -0,0 +1,46 @@
+package org.example.tests;
+import org.example.calculator.*;
+import org.example.comparator.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+public class ListComparatorTest {
+    @Test
+    public void testCompareFirstLarger() {
+        List<Integer> firstList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> secondList = Arrays.asList(2, 3, 4, 5, 6);
+
+        ListAverageCalculator firstListCalculator = new ListAverageCalculator(firstList);
+        ListAverageCalculator secondListCalculator = new ListAverageCalculator(secondList);
+
+        ListComparator listComparator = new ListComparator(firstListCalculator, secondListCalculator);
+
+        assertEquals("Первый список имеет большее среднее значение", listComparator.compare());
+    }
+    @Test
+    public void testCompareSecondLarger() {
+        List<Integer> firstList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> secondList = Arrays.asList(2, 3, 4, 5, 6);
+
+        ListAverageCalculator firstListCalculator = new ListAverageCalculator(firstList);
+        ListAverageCalculator secondListCalculator = new ListAverageCalculator(secondList);
+
+        ListComparator listComparator = new ListComparator(firstListCalculator, secondListCalculator);
+
+        assertEquals("Второй список имеет большее среднее значение", listComparator.compare());
+    }
+    @Test
+    public void testCompareEqualAverages() {
+        List<Integer> firstList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> secondList = Arrays.asList(2, 3, 4, 5, 6);
+
+        ListAverageCalculator firstListCalculator = new ListAverageCalculator(firstList);
+        ListAverageCalculator secondListCalculator = new ListAverageCalculator(secondList);
+
+        ListComparator listComparator = new ListComparator(firstListCalculator, secondListCalculator);
+
+        assertEquals("Средние значения равны", listComparator.compare());
+    }
+}
