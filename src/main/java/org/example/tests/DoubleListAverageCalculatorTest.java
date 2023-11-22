9c7d1ac4b3885f89cdd9072102a4623840f89863
Ivan Balabay <i.balabai@gmail.com>
2023-11-22 05:01:13 +0300

    package-info.java has been added, also Javadoc comments

--- src/main/java/org/example/tests/DoubleListAverageCalculatorTest.java
+++ src/main/java/org/example/tests/DoubleListAverageCalculatorTest.java
@@ -10,26 +10,41 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+/**
+ * Класс DoubleListAverageCalculatorTest содержит тесты для класса DoubleListAverageCalculator.
+ *
+ * @author Ivan
+ */
 public class DoubleListAverageCalculatorTest {
+    /**
+     * Тест проверяет, что среднее значение списка чисел с плавающей точкой корректно вычисляется.
+     */
     @Test
     public void testCalculateAverage() {
+        //CHECKSTYLE:OFF: checkstyle:magicnumber
         List<Double> numbers = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
         ListAverageCalculator calculator = new DoubleListAverageCalculator(numbers);
         double average = calculator.calculateAverage();
         assertEquals(3.0, average, 0.001);
+        //CHECKSTYLE:ON: checkstyle:magicnumber
     }
-
+    /**
+     * Тест проверяет, что среднее значение пустого списка равно 0.
+     */
     @Test
     public void testCalculateAverageZeroValue() {
         List<Double> numbers = List.of();
         ListAverageCalculator calculator = new DoubleListAverageCalculator(numbers);
         assertEquals(0, calculator.calculateAverage(), 0.0);
     }
-
+    /**
+     * Тест проверяет, что среднее значение не равно NaN.
+     */
     @Test
     public void testCalculateAverageNotNull() {
         List<Double> numbers = List.of();
         ListAverageCalculator calculator = new DoubleListAverageCalculator(numbers);
         assertFalse(Double.isNaN(calculator.calculateAverage()));
     }
+
 }
---
+++ src/main/java/org/example/tests/package-info.java
@@ -0,0 +1,10 @@
+package org.example.tests;
+/**
+ * Пакет содержит тесты для логики работы приложения.
+ * В пакте следующие классы:
+ * - TestRunner: запускает серию тестов, которые содержатся в классах: ListAverageCalculatorTest,
+ * DoubleAverageCalculatorTest, ListComparatorTest.
+ * Все классы предназначены для тестирования логики, реализованной в пакетах Calculator (вычисление)
+ * и Comparator (сравнение).
+ * Тест MainTest тестирует правильность работы консольного ввода.
+ */
